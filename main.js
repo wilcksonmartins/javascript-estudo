864a83ed48da494b582137c5fd6a0c90b3a43aff
wilckson.filho@gmail.com <wilckson.filho@gmail.com>
2022-06-27 00:09:04 -0300

    variáveis javascript

---
+++ main.js
@@ -0,0 +1,16 @@
+var nome = "wilkson";
+
+var idade = "19";
+
+var comidaFavorita;
+//declarar valor da variável da sua comida favorita//
+
+comidaFavorita = " peixe ";
+
+var escola,
+    cidade,
+    time,
+    filmeFavorito,
+    serieFavorita;
+    
+console.log(nome,idade,comidaFavorita,escola)
