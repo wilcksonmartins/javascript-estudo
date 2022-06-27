402ddc787c1d90742c0d3c16d7c7b964fcd4d694
wilckson.filho@gmail.com <wilckson.filho@gmail.com>
2022-06-27 01:30:05 -0300

    "aprendendo javascript"

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-var nome = "wilkson";
+// ESTUDO VARIÁVEL
+
+
+/**var nome = "wilkson";
 
 var idade = "19";
 
 var comidaFavorita;
 //declarar valor da variável da sua comida favorita//
 
-comidaFavorita = " peixe ";
+//comidaFavorita = " peixe ";//
 
 var escola,
     cidade,
@@ -13,4 +16,58 @@ var escola,
     filmeFavorito,
     serieFavorita;
     
-console.log(nome,idade,comidaFavorita,escola)
+console.log(nome,idade,comidaFavorita,escola**/
+
+
+// ESTUDO STRING//
+
+
+/*
+// Declare uma variável contendo uma string
+var string = "string";
+// Declare uma variável contendo um número dentro de uma string
+var numero ="18"
+// Declare uma variável com a sua idade
+var idade = 19;
+// Declare duas variáveis, uma com seu nome
+var nome = "wilkson";
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = "martins";
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "it's time"
+// Verifique o tipo da variável que contém o seu nome
+ 
+ console.log(typeof nome)*/
+ 
+ 
+ 
+ // ESTUDO NÚMEROS NA PROGRAMAÇÃO//
+ 
+ 
+ /* Qual o resultado da seguinte expressão?*/ var total = 10 + 5 + 2/2 + 20;
+ console.log(total);
+
+// Crie duas expressões que retornem NaN
+var expressao1 = "eu10";
+var expressao2 ="voce20" + 10;
+console.log(+expressao1);
+console.log(+expressao2);
+// Somar a string '200 com o número 50 e retornar 250
+var soma1 = +"200" + 50;
+ console.log(soma1);
+// Incremente o número 5 e retorne o seu valor incrementado
+var incrementar = 5;
+
+console.log(++incrementar)
+
+// Como dividir o peso por 2?
+
+var numero= +'80' /2;
+
+var unidade = 'kg';
+
+var peso = numero + unidade; // '80kg'
+
+var divisao = peso /2
+
+console.log(peso)
